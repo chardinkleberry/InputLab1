a80054bea765bd0ab50ef63fb651e9a1a5302593
Charlie Liu <100143091@lubbockisd.net>
2023-09-14 09:27:52 -0500

    InputLab (Charlie Liu)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,56 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class Main
+{
+    public static void main (String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int intOne, intTwo;
+        float floatOne, floatTwo;
+        double doubleOne, doubleTwo;
+
+
+        System.out.print("Enter an integer :: ");
+        intOne = keyboard.nextInt();
+
+
+        System.out.print("Enter an integer :: ");
+        intTwo = keyboard.nextInt();
+
+
+        System.out.print("Enter an float :: ");
+        floatOne = keyboard.nextFloat();
+
+
+        System.out.print("Enter an float :: ");
+        floatTwo = keyboard.nextFloat();
+
+        System.out.print("Enter an double :: ");
+        doubleOne = keyboard.nextDouble();
+
+
+        System.out.print("Enter an double :: ");
+        doubleTwo = keyboard.nextDouble();
+
+
+        //add in input for all variables
+
+
+        System.out.println();
+        System.out.println("integer one = " + intOne );
+        System.out.println("integer two = " + intTwo );
+
+        System.out.println();
+        System.out.println("float one = " + floatOne );
+        System.out.println("float two = " + floatTwo );
+
+        System.out.println();
+        System.out.println("double one = " + doubleOne );
+        System.out.println("double two = " + doubleTwo );
+
+        //add output for all variables
+
+
     }
 }
